ad7484b0380531077aa2c6487660a923c39570de
PMcca <petemcca@gmail.com>
2021-11-24 13:11:29 +0000

    Cleans up main

--- src/main/java/org/pmcca/kingtest/Main.java
+++ src/main/java/org/pmcca/kingtest/Main.java
@@ -1,9 +1,6 @@
 package org.pmcca.kingtest;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.pmcca.kingtest.server.Server;
 import org.slf4j.Logger;
@@ -16,37 +13,5 @@ public class Main {
   public static void main(String[] args) throws IOException {
     Server server = new Server(8000, 200, 100);
     server.listenAndServe();
-  }
-  // TODO clean up main
-  static class MyHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange t) throws IOException {
-      int id = counter.getAndIncrement();
-      log.info("Got request {}", id);
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-
-      }
-      log.info("Returning request {}", id);
-      String response = "" + id + " ";
-      t.sendResponseHeaders(200, response.length());
-      OutputStream os = t.getResponseBody();
-      os.write(response.getBytes());
-      os.close();
-    }
-  }
-
-  static class MyHandlerB implements HttpHandler {
-    @Override
-    public void handle(HttpExchange t) throws IOException {
-      log.info("In handlerB");
-
-      String response = "in handler b";
-      t.sendResponseHeaders(200, response.length());
-      OutputStream os = t.getResponseBody();
-      os.write(response.getBytes());
-      os.close();
-    }
   }
 }
